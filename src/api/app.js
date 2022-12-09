41f6fe37fc4d97dfc5eeb9e094173a4f3fda4592
Tarsis de Lima <br.tmvdl@gmail.com>
2022-12-09 14:06:37 -0300

    Fixing routes in API app file

--- src/api/app.js
+++ src/api/app.js
@@ -1,10 +1,27 @@
-
 const { router, sh, system } = require('./libs')
 
-router.route('login', ({ user, password }) => system.login(user, password))
+// system
 
-router.route('createuser', ({ name, password, host }) => sh.createuser({ name, password, host }))
+router.route('system.login', ({ user, password }) => system.login({ user, password }))
 
-router.route('describeuser', ({ name, host }) => sh.describeuser({ name, host }))
+// user
+
+router.route('user.create', ({ name, password, host }) => sh.user.create({ name, password, host }))
+
+router.route('user.delete', ({ id }) => sh.user.delete({ id }))
+
+router.route('user.describe', ({ id }) => sh.user.describe({ id }))
+
+// data
+
+router.route('data.list', ({ id, where, groupby, order }) => sh.data.list({ id, where, groupby, order }))
+
+router.route('data.create', ({ data }) => sh.data.create({ data }))
+
+router.route('data.describe', ({ id }) => sh.data.describe({ id }))
+
+router.route('data.update', ({ id, data }) => sh.data.update({ id, data }))
+
+router.route('data.delete', ({ id }) => sh.data.delete({ id }))
 
 module.exports = router
