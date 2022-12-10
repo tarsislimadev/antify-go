ef87f978ce698cd899fe3cb876f6e8a91f9828b4
Tarsis de Lima <br.tmvdl@gmail.com>
2022-12-10 14:50:48 -0300

    Fixing exceptions files

--- src/api/libs/exceptions.js
+++ src/api/libs/exceptions.js
@@ -1,31 +1,40 @@
-
 class ValidationError extends Error {
   constructor(message = '') {
-    super('VALIDATION ' + message)
+    const messages = ['VALIDATION']
+    if (message) messages.push(message)
+    super(messages.join(' - '))
   }
 }
 
 class NotImplementedError extends Error {
   constructor(message = '') {
-    super('NOT IMPLEMENTED - ' + message)
+    const messages = ['NOT IMPLEMENTED']
+    if (message) messages.push(message)
+    super(messages.join(' - '))
   }
 }
 
 class DuplicatedFunctionError extends Error {
-  constructor(func = '') {
-    super('DUPLICATED FUNCTION - ' + func)
+  constructor(message = '') {
+    const messages = ['DUPLICATED FUNCTION']
+    if (message) messages.push(message)
+    super(messages.join(' - '))
   }
 }
 
 class UndefinedFunctionError extends Error {
-  constructor(func = '') {
-    super('UNDEFINED FUNCTION - ' + func)
+  constructor(message = '') {
+    const messages = ['UNDEFINED FUNCTION']
+    if (message) messages.push(message)
+    super(messages.join(' - '))
   }
 }
 
 class NotAFunctionError extends Error {
-  constructor(func = '') {
-    super('NOT A FUNCTION - ' + func)
+  constructor(message = '') {
+    const messages = ['NOT A FUNCTION']
+    if (message) messages.push(message)
+    super(messages.join(' - '))
   }
 }
 
