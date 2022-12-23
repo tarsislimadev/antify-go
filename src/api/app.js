23bbf9f97444d75b70dbef299af67e3932a27e1e
Tarsis <br.tmvdl@gmail.com>
2022-12-23 20:44:03 -0300

    Fixing application files

--- src/api/app.js
+++ src/api/app.js
@@ -1,5 +1,15 @@
-const run = (req, res) => res.json({ })
+const { routes } = require('./routes')
+const { logger } = require('./utils')
 
-module.exports = {
-  run
+const run = (req, res) => {
+  logger('app/run', { req, res })
+
+  // FIXME
+  
+  // return routes.filter(r => r.name == req.path)
+  //   .reduce((p, res) => route.doer.func(req, res) || res, res)
+
+  return res.toString()
 }
+
+module.exports = { run }
--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 const netPkg = require('net')
-const { port: PORT } = require('./config')
+const { PORT } = require('./config')
 
 const app = require('./app')
 const Request = require('./request')
@@ -7,12 +7,15 @@ const Response = require('./response')
 
 const server = netPkg.createServer((listener) => {
   listener.on('data', (chunk) => {
-    listener.end(
-      app.run(
-        new Request({ chunk }),
-        new Response({})
-      )
-    )
+    const response = new Response({})
+
+    try {
+      response.fill(app.run(new Request({ chunk }), new Response({})))
+    } catch (e) {
+      response.setError(e)
+    }
+
+    listener.end(response.toString())
   })
 })
 
