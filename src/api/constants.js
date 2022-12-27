af9ae6e459306fae9e629c25a4251c971dcf250f
Tarsis <br.tmvdl@gmail.com>
2022-12-27 18:03:42 -0300

    Fixing CONSTANTS file

--- src/api/constants.js
+++ src/api/constants.js
@@ -3,10 +3,18 @@ module.exports = {
   EMPTY: '',
   BLANK: ' ',
   NEWLINE: '\r\n',
+  SLASH: '/',
   EQUALS: '=',
   INTERROGATION: '?',
   E_COMMERCIAL: '&',
   REGEXP: {
     NEWLINE: /\r\n/ig
   },
+  STATUS: {
+    OK: 200,
+    CLIENT_ERROR: 400,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    SERVER_ERROR: 500,
+  }
 }
