216d92cf5eeee93f5686573fffa8b9456ff764a8
Tarsis <br.tmvdl@gmail.com>
2022-12-27 17:58:37 -0300

    Removing useless files

--- src/api/utils.js
+++
@@ -1,9 +0,0 @@
-const { ENV } = require('./config')
-
-const logger = (name, log = {}) => {
-  if (process.env.ENV == 'development') {
-    console.log(name, log)
-  }
-}
-
-module.exports = { logger }
