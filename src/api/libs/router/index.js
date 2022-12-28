ee7e4072b851f64d7837a5e8d6ca48de6c9f175b
Tarsis <br.tmvdl@gmail.com>
2022-12-28 17:37:36 -0300

    Adding Router library

---
+++ src/api/libs/router/index.js
@@ -0,0 +1,27 @@
+const { info } = require('../logger')
+
+class Doer {
+  func = null
+
+  do(f = (() => { })) {
+    this.func = f
+  }
+
+  doing(req, res) {
+    return this.func(req, res)
+  }
+}
+
+class Router {
+  routes = []
+
+  use(name = '') {
+    info('Router.use', { name })
+
+    const doer = new Doer(name)
+    this.routes.push({ name, doer })
+    return doer
+  }
+}
+
+module.exports = new Router()
