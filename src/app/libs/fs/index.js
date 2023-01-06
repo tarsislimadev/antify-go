5767b938c5227eb78d808f94fd98634bfa95daf8
Tarsis <br.tmvdl@gmail.com>
2023-01-06 19:39:16 -0300

    Fixing File System library

--- src/app/libs/fs/index.js
+++ src/app/libs/fs/index.js
@@ -1,11 +1,14 @@
 const fs = require('fs')
 const path = require('path')
 
-const mkdirSync = (...name) => fs.mkdirSync(path.resolve(...name), { recursive: true })
+const mkdirSync = (name) => fs.mkdirSync(path.resolve(name), { recursive: true })
 
 const writeFileSync = (name, value = '') => fs.writeFileSync(name, value)
 
-const readdirSync = (...name) => fs.readdirSync(path.resolve(...name))
+const readdirSync = (name) => {
+  mkdirSync(name)
+  fs.readdirSync(path.resolve(name))
+}
 
 module.exports = {
   mkdirSync,
