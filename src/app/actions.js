75384d1e33d20d5497cd0de6d9831a25b8ef5031
Tarsis <br.tmvdl@gmail.com>
2023-01-06 19:40:55 -0300

    Fixing actions

--- src/app/actions.js
+++ src/app/actions.js
@@ -1,57 +1,164 @@
 const db = require('./libs/db')
+const logger = require('./libs/logger')
 
-const login = ({ username, password }) => {
+const login = ({
+  username = null,
+  password = null,
+}) => {
+  logger.info('actions/login', { username, password })
+
   const { id: token } = db.in('logins').new().writeMany({ username, password })
   return { token }
 }
 
-const createuser = ({ token = null, username = null, password = null }) => {
+const createuser = ({
+  token = null,
+  username = null,
+  password = null,
+}) => {
+  logger.info('actions/createuser', { token, username, password })
+
   const login = db.in('logins').find(token)
 
-  if (!login) throw new Error('Login not found.', { token })
+  if (!login) {
+    throw new Error('Login not found.', { token, username, password })
+  }
 
   const { id } = db.in('users').new().writeMany({
     username,
     password,
-    login: login.id
+    login_id: login.id,
   })
 
   return { id }
 }
 
-const describeuser = ({ token = null, username = null }) => ({})
+const describeuser = ({
+  token = null,
+  username = null,
+}) => {
+  logger.info('actions/describeuser', { token, username })
+}
 
-const grantuser = ({ token = null, perm = null, username = null }) => ({})
+const grantuser = ({
+  token = null,
+  perm = null,
+  username = null,
+}) => {
+  logger.info('actions/grantuser', { token, perm, username })
+}
 
-const revokeuser = ({ token = null, perm = null, username = null }) => ({})
+const revokeuser = ({
+  token = null,
+  perm = null,
+  username = null,
+}) => {
+  logger.info('actions/revokeuser', { token, perm, username })
+}
 
-const deleteuser = ({ token = null, username = null }) => ({})
+const deleteuser = ({
+  token = null,
+  username = null,
+}) => {
+  logger.info('actions/deleteuser', { token, username })
+}
 
-const deleteallusers = ({ token = null }) => ({})
+const deleteallusers = ({
+  token = null,
+}) => {
+  logger.info('actions/deleteallusers', { token })
+}
 
-const listallusers = ({ token = null }) => ({})
+const listallusers = ({
+  token = null,
+}) => {
+  logger.info('actions/listallusers', { token })
+}
 
-const createdir = ({ token = null, dirname = null, recursive = null, public = null }) => ({})
+const createdir = ({
+  token = null,
+  dirname = null,
+  recursive = null,
+  public = null,
+}) => {
+  logger.info('actions/createdir', { token, dirname, recursive, public })
+}
 
-const listdir = ({ token = null, dirname = null }) => ({})
+const listdir = ({
+  token = null,
+  dirname = null,
+}) => {
+  logger.info('actions/listdir', { token, dirname })
+}
 
-const publicdir = ({ token = null, dirname = null, readonly = null }) => ({})
+const publicdir = ({
+  token = null,
+  dirname = null,
+  readonly = null,
+}) => {
+  logger.info('actions/publicdir', { token, dirname, readonly })
+}
 
-const privatedir = ({ token = null, dirname = null }) => ({})
+const privatedir = ({
+  token = null,
+  dirname = null,
+}) => {
+  logger.info('actions/privatedir', { token, dirname })
+}
 
-const deletedir = ({ token = null, dirname = null, force = null }) => ({})
+const deletedir = ({
+  token = null,
+  dirname = null,
+  force = null,
+}) => {
+  logger.info('actions/deletedir', { token, dirname, force })
+}
 
-const createfile = ({ token = null, filename = null, recursive = null }) => ({})
+const createfile = ({
+  token = null,
+  filename = null,
+  recursive = null,
+}) => {
+  logger.info('actions/createfile', { token, filename, recursive })
+}
 
-const listfile = ({ token = null, filename = null, hidden = null }) => ({})
+const listfile = ({
+  token = null,
+  filename = null,
+  hidden = null,
+}) => {
+  logger.info('actions/listfile', { token, filename, hidden })
+}
 
-const readfile = ({ token = null, filename = null }) => ({})
+const readfile = ({
+  token = null,
+  filename = null,
+}) => {
+  logger.info('actions/readfile', { token, filename })
+}
 
-const writetext = ({ token = null, filename = null, content = null }) => ({})
+const writetext = ({
+  token = null,
+  filename = null,
+  content = null,
+}) => {
+  logger.info('actions/writetext', { token, filename, content })
+}
 
-const writefile = ({ token = null, filename = null }) => ({})
+const writefile = ({
+  token = null,
+  filename = null,
+}) => {
+  logger.info('actions/writefile', { token, filename })
+}
 
-const deletefile = ({ token = null, filename = null, force = null }) => ({})
+const deletefile = ({
+  token = null,
+  filename = null,
+  force = null,
+}) => {
+  logger.info('actions/deletefile', { token, filename, force })
+}
 
 module.exports = {
   login,
