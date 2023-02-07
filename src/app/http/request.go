db2167bb4bc67faec7dbff65697f7d38f6093beb
Tarsis de Lima <br.tmvdl@gmail.com>
2023-02-07 10:30:46 -0300

    Fixing HTTP library files

--- src/app/http/request.go
+++ src/app/http/request.go
@@ -1,36 +1,6 @@
 package antify
 
-import "encoding/json"
 import "strings"
-import "log"
-
-const BLANK = " "
-const LINE_BREAK = "\r\n"
-const CONTENT_TYPE = "Content-Type"
-
-func GetStatusMessage(status string) string {
-	switch status {
-		case "200": return "OK";
-		case "404": return "NOT FOUND";
-	}
-
-	return "ERROR";
-}
-
-func GetFirstLine(status string) string {
-	message := GetStatusMessage(status)
-	return strings.Join([]string{"HTTP/1.1", status, message}, BLANK)
-}
-
-func GetContentType() string {
-	return strings.Join([]string{CONTENT_TYPE, "application/json"}, ": ")
-}
-
-func GetJSONString(obejct any) string {
-	bytes, err := json.Marshal(obejct)
-	log.Panic(err)
-	return string(bytes)
-}
 
 type Request struct {
 	Method string
--- src/app/http/response.go
+++ src/app/http/response.go
@@ -1,7 +1,59 @@
 package antify
 
-type Response struct {
+import "log"
+import "strings"
+import "encoding/json"
+
+type ResponseBody struct {
 	Status string `json:"status"`
 	Message string `json:"message"`
 	Data map[string]interface{} `json:"data"`
+}
+
+func (body ResponseBody) ToString() string {
+	str, err := json.Marshal(body)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return string(str)
+}
+
+type Response struct {
+	Status string
+	ContentType string
+	Body ResponseBody
+}
+
+func (res Response) ToString() string {
+	str := make([]string, 0)
+
+	str = append(str, GetFirstLine(res.Status))
+
+	str = append(str, GetContentType())
+
+	str = append(str, "")
+
+	str = append(str, res.Body.ToString())
+
+	return strings.Join(str, LINE_BREAK)
+}
+
+func GetStatusMessage(status string) string {
+	switch status {
+		case "200": return "OK";
+		case "404": return "NOT FOUND";
+	}
+
+	return "ERROR";
+}
+
+func GetFirstLine(status string) string {
+	message := GetStatusMessage(status)
+	return strings.Join([]string{"HTTP/1.1", status, message}, BLANK)
+}
+
+func GetContentType() string {
+	return strings.Join([]string{CONTENT_TYPE, "application/json"}, ": ")
 }
