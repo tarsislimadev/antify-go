f3b898896d9887601152c516727e0747caf53b56
Tarsis de Lima <br.tmvdl@gmail.com>
2023-02-08 16:26:16 -0300

    Fixing LOGIN action

--- src/app/actions/login.go
+++ src/app/actions/login.go
@@ -1,8 +1,19 @@
 package antify
 
+import "os"
 import http "github.com/brtmvdl/antify/http"
+import database "github.com/brtmvdl/antify/database"
 
 func Login (req http.Request, res http.Response) http.Response {
-	res.AddHeader("Method", "Login")
+	db := database.Database{os.Getenv("ANTIFY_PATH")}
+	login := db.In("logins").New()
+
+	login.WriteMany(map[string][]byte{
+		"username": []byte(req.Query["username"]),
+		"password": []byte(req.Query["password"]),
+	})
+
+	res.Body = http.ResponseBody{"ok", "", map[string]interface{}}
+
 	return res
 }
