95b8085aafbb88a86966f7587d432b890aebcd4c
Tarsis de Lima <br.tmvdl@gmail.com>
2023-02-08 16:25:39 -0300

    Adding DATABASE library

---
+++ src/app/database/database.go
@@ -0,0 +1,61 @@
+package antify
+
+import "os"
+import "strings"
+import uuid "github.com/brtmvdl/antify/uuid"
+
+const SLASH = "/"
+
+type Database struct {
+	DataPath string
+}
+
+// const DEFAULT = Database{
+// 	DataPath: os.Getenv("ANTIFY_PATH"),
+// }
+
+func (db Database) In(dataPath string) Database {
+	return Database{
+		DataPath: strings.Join([]string{db.DataPath, dataPath}, SLASH),
+	}
+}
+
+func (db Database) New() DatabaseObject {
+	obj := DatabaseObject{
+		Database: db,
+		Id: uuid.New(),
+	}
+
+	obj.CreatePath()
+
+	return obj
+}
+
+type DatabaseObject struct {
+	Database
+	Id string
+}
+
+func (obj DatabaseObject) CreatePath() error {
+	path := strings.Join([]string{obj.Database.DataPath, obj.Id}, SLASH)
+
+	return os.MkdirAll(path, 0644)
+}
+
+func (obj DatabaseObject) WriteMany(props map[string][]byte) []error {
+	errors := make([]error, 0)
+
+	for key, value := range props {
+		err := obj.Write(key, value)
+
+		if err != nil {
+			errors = append(errors, err)
+		}
+	}
+
+	return errors
+}
+
+func (obj DatabaseObject) Write(name string, value []byte) error {
+	return nil
+}
