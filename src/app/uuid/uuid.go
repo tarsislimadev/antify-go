e0dd79a1e867412dc3a50a096d18747fb88889bc
Tarsis de Lima <br.tmvdl@gmail.com>
2023-02-09 15:17:58 -0300

    Fixing UUID library

--- src/app/uuid/uuid.go
+++ src/app/uuid/uuid.go
@@ -1,9 +1,5 @@
 package antify
 
-import "time"
-
 func New() string {
-	then := time.Date(2023, 02, 8, 18, 00, 00, 0, time.UTC)
-
-	return string(time.Now().Sub(then).Nanoseconds())
+	return string(1) // FIXME
 }
